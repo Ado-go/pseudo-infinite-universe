ccc50558e55d344ad33cfae8ebfab561a4907955
Ado-go <131666442+Ado-go@users.noreply.github.com>
2023-08-10 18:24:03 +0200

    add initial randomness, better color picking and cleaner code

--- universe.py
+++ universe.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from random import randint
 
 Width = 500
 Height = 500
@@ -21,29 +22,36 @@ class Universe:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.randomness = randint(0, 900_000_000)
         self.up = 0
         self.left = 0
         self.moving = False
 
-    def create_stars(self):
+    def draw(self):
         canvas.delete("all")
+        self.create_stars()
+        self.draw_coordinates()
+
+    def draw_coordinates(self):
+        canvas.create_text(20 + len(str(self.x)) * 9, 20, text="X: {}".format(self.x), font=("Arial", 25), fill="white")
+        canvas.create_text(20 + len(str(self.y)) * 9, 50, text="Y: {}".format(self.y), font=("Arial", 25), fill="white")
+
+    def create_stars(self):
         col = -10
         for i in range(Width // 30):
             col += 30
             row = 0
             for j in range(Height // 30):
                 row += 14
-                random_number = lehmer_random_number(((i + self.x) & 16777215) << 64 | ((j + self.y) & 16777215))
+                random_number = lehmer_random_number(((i + self.x) & 16777215) << 64 | ((j + self.y) & 16777215)
+                                                     + self.randomness)
                 if random_number % 11 == 10:
                     size = random_number % 26
                     canvas.create_oval(i + col - size // 2 - 15,
                                        j + row * 2 - size // 2 - 10,
                                        i + size + col - size // 2 - 10,
                                        j + size + row * 2 - size // 2 - 5,
-                                       outline="white", fill=random_color(random_number))
-                    canvas.create_text(110, 20, text="X: {} Y: {} ".format(self.x, self.y),
-                                       font=("Arial", 25),
-                                       fill="white")
+                                       fill=random_color(random_number))
 
     def movement(self, event):
         if event.keysym == "a":
@@ -60,7 +68,7 @@ class Universe:
         if self.moving:
             self.x += self.left
             self.y += self.up
-            self.create_stars()
+            self.draw()
         canvas.after(10, self.space_travel)
 
     def stop_moving(self, event):
@@ -71,35 +79,37 @@ class Universe:
         if self.up == 0 and self.left == 0:
             self.moving = False
 
+    def draw_solar_system(self, random_number):
+        canvas.create_rectangle(0, Height // 2, Width, Height, outline="lightblue", fill="lightblue")
+        canvas.create_oval(10, Height // 2 + Height // 4, 60, Height // 2 + Height // 4 + 50,
+                           fill=random_color(random_number))
+
+        canvas.create_text(45, Height // 2 + Height // 4 - 30, text="Star " + str(random_number % 999_999_999))
+
+        number_of_planets = random_number % 10
+        space = 0
+        for i in range(number_of_planets):
+            moon_space = 0
+            number_of_moons = (random_number + i) % 4
+            space += 35
+            canvas.create_oval(65 + space, Height // 2 + Height // 4 + 10,
+                               65 + space + 30, Height // 2 + Height // 4 + 40,
+                               fill=random_color(random_number << i + 1))
+            for j in range(number_of_moons):
+                moon_space += 22
+                canvas.create_oval(70 + space,
+                                   Height // 2 + Height // 4 + 25 + moon_space,
+                                   65 + space + 25,
+                                   Height // 2 + Height // 4 + 45 + moon_space,
+                                   fill=random_color(random_number << j + 1, True))
+
     def hover_over_planet(self, event):
         x = event.x // 30
         y = event.y // 30
-        random_number = lehmer_random_number(((x + self.x) & 16777215) << 64 | ((y + self.y) & 16777215))
+        random_number = lehmer_random_number(((x + self.x) & 16_777_215) << 64 | ((y + self.y) & 16_777_215)
+                                             + self.randomness)
         if random_number % 11 == 10:
-            canvas.create_rectangle(0, Height//2, Width, Height,
-                                    outline="lightblue", fill="lightblue")
-            canvas.create_oval(10, Height // 2 + Height // 4,
-                               60, Height // 2 + Height // 4 + 50,
-                               fill=random_color(random_number))
-            num = random_number % 999999999
-            canvas.create_text(45, Height // 2 + Height // 4 - 30,
-                               text="Star " + str(num))
-            number_of_planets = random_number % 10
-            space = 0
-            for i in range(number_of_planets):
-                moon_space = 0
-                number_of_moons = (random_number + i) % 4
-                space += 35
-                canvas.create_oval(65 + space, Height // 2 + Height // 4 + 10,
-                                   65 + space + 30, Height // 2 + Height // 4 + 40,
-                                   fill=random_color(random_number << i))
-                for j in range(number_of_moons):
-                    moon_space += 22
-                    canvas.create_oval(70 + space,
-                                       Height // 2 + Height // 4 + 25 + moon_space,
-                                       65 + space + 25,
-                                       Height // 2 + Height // 4 + 45 + moon_space,
-                                       fill=random_color(random_number, True))
+            self.draw_solar_system(random_number)
 
 
 def random_color(random_number, moon=False):
@@ -107,17 +117,18 @@ def random_color(random_number, moon=False):
                      "8", "9", "a", "b", "c", "d", "e", "f"]
     color = "#"
     for i in range(6):
+        new_random_number = lehmer_random_number(random_number + i + 100)
         if moon:
-            x = color_palette[random_number % 16]
-            y = color_palette[(random_number + 5) % 16]
+            x = color_palette[new_random_number % 16]
+            y = color_palette[new_random_number % 16]
             color += x+y+x+y+x+y
             return color
-        color += color_palette[(random_number ^ i) % 16]
+        color += color_palette[new_random_number % 16]
     return color
 
 
 universe = Universe()
-universe.create_stars()
+universe.draw()
 
 canvas.after(10, universe.space_travel)
 canvas.bind("<Button-1>", lambda e: universe.hover_over_planet(e))
